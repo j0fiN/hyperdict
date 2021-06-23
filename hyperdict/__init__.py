917d28bdcef78f93e2c63cbd29106e9a2fb934d2
Jofin <jofinfab@gmail.com>
2021-06-23 21:47:05 +0530

    feat: :hammer: :white_check_mark:Added hyperdict module and initialized tests.
    Message body
    - Developements scripts added.
    - pytest added
    - A todo added for updating works

---
+++ hyperdict/__init__.py
@@ -0,0 +1,12 @@
+
+# __init__
+# ~~~~~~~
+# init of hyperdict
+
+from .hyperdict import HyperDict
+__version__ = '1.0.0'
+
+
+# from hyperdict import HyperDict as HD
+# n = HD()
+# n['name', 'age'] = n.each('magnus', 32)
